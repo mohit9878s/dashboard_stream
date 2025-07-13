80e2b453bd6c57a85683ec297631effe62e36838
Mohit <121569986+mohit9878s@users.noreply.github.com>
2025-07-13 13:08:18 +0530

    Update jarvis_logo.py

--- jarvis_logo.py
+++ jarvis_logo.py
@@ -1,14 +1,3 @@
-# import base64
-# def image_to_base64(img_path):
-#     with open(img_path, "rb") as image_file:
-#         return base64.b64encode(image_file.read()).decode()
-# def jarvis_logo():
-#     # Paths
-#     # Change the path to your image file
-#     jarvis_path = r"jarvis_Logo_1.png"  # Update this path as needed
-#     jarvis_base64 = image_to_base64(jarvis_path)
-# if __name__ == "__main__":
-#     jarvis_logo()
 
 # jarvis_logo.py
 import base64
@@ -23,8 +12,8 @@ def jarvis_logo():
     return jarvis_base64
 
 if __name__ == "__main__":
-    print(jarvis_logo())  # Optional testing
-
+#    print(jarvis_logo())  # Optional testing
+    jarvis_logo()
 
 
 
