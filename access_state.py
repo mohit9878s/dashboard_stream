0dcdd15f65d972dd73c34ca2b81c9aed60f37503
Mohit <121569986+mohit9878s@users.noreply.github.com>
2025-07-22 22:50:09 +0530

    Update access_state.py

--- access_state.py
+++ access_state.py
@@ -329,7 +329,7 @@ with st.sidebar:
     # st.warning("Please select **only one Communication Type (OBD or WhatsApp)** to view remarks.")
 
    # st.markdown("### 📨 Type of Communication")
-    comm_options = sorted(df["Type of Communication"].dropna().unique())
+comm_options = sorted(df["Type of Communication"].dropna().unique())
 comm_selected = st.pills("Filter Communication Types", comm_options, selection_mode="multi")
 
 
